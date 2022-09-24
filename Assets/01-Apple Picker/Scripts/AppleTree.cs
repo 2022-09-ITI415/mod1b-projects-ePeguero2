b188de38876f0fb9a6d13384d546f42c438446fc
Estifferson Peguero <45677818+ePeguero2@users.noreply.github.com>
2022-09-23 23:32:19 -0700

    Basic Movement Done

--- Assets/01-Apple Picker/Scripts/AppleTree.cs
+++ Assets/01-Apple Picker/Scripts/AppleTree.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 20f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        //Changing Direction
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed); //Move right
+        }
+        else if (pos.x > leftAndRightEdge)
+        {
+            speed = -Mathf.Abs(speed); //Move left
+        }
+    }
+    void FixedUpdate()
+    {
+        // Changing Direction Randomly is now time-based because of this function
+        if (Random.value < chanceToChangeDirections)
+        {
+            speed *= -1; //change direction
+        }
     }
 }
