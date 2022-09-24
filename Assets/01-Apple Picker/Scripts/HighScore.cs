ce3d1fc3fdaf4a0af055921e714881e39973932e
Estifferson Peguero <45677818+ePeguero2@users.noreply.github.com>
2022-09-24 14:03:36 -0700

    Prototype Ready

--- Assets/01-Apple Picker/Scripts/HighScore.cs
+++ Assets/01-Apple Picker/Scripts/HighScore.cs
@@ -5,18 +5,31 @@ using UnityEngine.UI;
 
 public class HighScore : MonoBehaviour
 {
-    static public int score = 1000;
+    static public int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
+    void Awake()
+    {
+        if (PlayerPrefs.HasKey("HighScore"))
+        {
+            score = PlayerPrefs.GetInt("HighScore");
+        }
+        PlayerPrefs.SetInt("HighScore", score);
+    }
+    
     // Update is called once per frame
     void Update()
     {
         Text gt = this.GetComponent<Text>();
         gt.text = "High Score: " + score;
+        if (score > PlayerPrefs.GetInt("HighScore"))
+        {
+            PlayerPrefs.SetInt("HighScore", score);
+        }
     }
 }
