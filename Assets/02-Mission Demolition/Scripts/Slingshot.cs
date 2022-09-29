9703da80ff5d959cc574fbce643527c084e5bea7
Estifferson Peguero <45677818+ePeguero2@users.noreply.github.com>
2022-09-29 18:08:11 -0400

    Halo turns on and off

---
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject launchPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+    void OnMouseEnter()
+    {
+        //print("Singshot : OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    void OnMouseExit()
+    {
+        //print("Slingshot : OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
