e81a96508ac97421665bb64054efcf8c0bff770f
Estifferson Peguero <45677818+ePeguero2@users.noreply.github.com>
2022-10-09 19:09:18 -0700

    Player Movement

--- Assets/03-Prototype1/Scripts/CamFlow.cs
+++ Assets/03-Prototype1/Scripts/CamFlow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlowCam : MonoBehaviour
+public class CamFlow : MonoBehaviour
 {
     public GameObject player;
 
