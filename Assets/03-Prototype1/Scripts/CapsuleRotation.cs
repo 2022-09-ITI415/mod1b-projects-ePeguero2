56c41ae2b6b606ebda826f5875f5842ee5ba757e
Estifferson Peguero <45677818+ePeguero2@users.noreply.github.com>
2022-10-10 19:55:57 -0700

    done

--- Assets/03-Prototype1/Scripts/CapsuleRotation.cs
+++ Assets/03-Prototype1/Scripts/CapsuleRotation.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotation : MonoBehaviour
+public class CapsuleRotation : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
--- Assets/03-Prototype1/Scripts/ThePlayer.cs
+++ Assets/03-Prototype1/Scripts/ThePlayer.cs
@@ -2,19 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using TMPro;
 public class ThePlayer : MonoBehaviour
 {
-
+    public TextMeshProUGUI countText;
     private Rigidbody rb;
     public float speed = 0;
     private float movementX;
     private float movementY;
+    private int count;
+    public GameObject winTextObject;
 
     // Start is called before the first frame update
     void Start()
-    {
+    { 
         rb = GetComponent<Rigidbody>();
+
+        count = 0;
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     private void OnMove(InputValue movementValue)
@@ -23,6 +30,15 @@ public class ThePlayer : MonoBehaviour
 
         movementX = movementVector.x;
         movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Capsules: " + count.ToString();
+        if (count >= 22)
+        {
+            winTextObject.SetActive(true);
+        }
     }
 
     private void FixedUpdate()
@@ -37,8 +53,11 @@ public class ThePlayer : MonoBehaviour
         if (other.gameObject.CompareTag("Capsule"))
         {
             other.gameObject.SetActive(false);
-        }
 
+            count = count + 1;
+
+            SetCountText();
+        }
     }
     // Update is called once per frame
     void Update()
